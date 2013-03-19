13be0968ebff83ef85c1305a236d180f84f01e2e
Schuyler Goodwin <schuylergoodwin@gmail.com>
2013-03-19 13:57:19 -0400

    BLUE commit

--- src/edu/elon/cs/locationexposure/LocationExposureActivity.java
+++ src/edu/elon/cs/locationexposure/LocationExposureActivity.java
@@ -17,7 +17,7 @@ public class LocationExposureActivity extends Activity {
 	private RelativeLayout relativeLayout; 
 	private LocationManager locManager; 
 	
-	private int[] colors = {Color.RED, Color.GREEN}; 
+	private int[] colors = {Color.RED, Color.BLUE}; 
 	
 	//which color/section of array, showing
 	private int which; 
